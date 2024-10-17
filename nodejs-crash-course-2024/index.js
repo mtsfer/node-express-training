7fb556d7d0ef438bf079c9d28a7f9936f72a20a0
mtsfer <mtshs.ferraz@gmail.com>
2024-10-17 20:35:57 -0300

    Add nodejs-crash-course-2024 files

---
+++ nodejs-crash-course-2024/index.js
@@ -0,0 +1,11 @@
+import getPosts, { getPostLength } from "./post-controller.js";
+
+console.log(getPosts());
+
+console.log(`Posts Length: ${getPostLength()}`);
+
+// const { generateRandomNumber, celsiusToFahrenheit } = require('./utils');
+
+// console.log(`Random Number: ${generateRandomNumber()}`);
+
+// console.log(`Celsius to Fahrenheit: ${celsiusToFahrenheit(0)}`);
